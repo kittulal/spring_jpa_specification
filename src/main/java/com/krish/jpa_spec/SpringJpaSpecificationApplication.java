dd37fab918de4cccc96c49684351d57ca7660c52
kittulal <punnakrishnaa@gmail.com>
2026-01-17 11:00:09 +0530

    first commit

---
+++ src/main/java/com/krish/jpa_spec/SpringJpaSpecificationApplication.java
@@ -0,0 +1,13 @@
+package com.krish.jpa_spec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaSpecificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpaSpecificationApplication.class, args);
+	}
+
+}
